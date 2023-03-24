2303221f2e685dae2301b8a248f9eb93d5d02e8b
zZ7_aki <zakariae.bouklah18@gmail.com>
2023-03-24 17:22:38 +0000

    setting up the UI

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Flight } from '../models/flight.model';
+import { FlightsService } from '../services/flights.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit{
+
+  flights: Flight[] = [];
+
+  isEditing: boolean = false;
+  isDeleting: boolean = false;
+  isCreating: boolean = false;
+
+  /*
+    form fields
+  */
+
+  origin: string;
+  destination: string;
+  flightNumber: number;
+  departure: Date;
+  arrival: Date;
+  nonstop: boolean;
+
+  constructor(private flightService: FlightsService) {}
+
+  ngOnInit(): void {
+    this.flightService.getFlights().subscribe((data) => {
+      this.flights = data
+    })
+  }
+
+  handleDelete() : void
+  {
+    this.isDeleting = true;
+  }
+
+  handleEdit() : void
+  {
+    this.isEditing = true;
+  }
+
+  handleNew(): void
+  {
+    this.isCreating = true;
+  }
 
 }
